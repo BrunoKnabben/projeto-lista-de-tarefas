05ad52bba7468d64b3db6b551d739a86694d4770
BrunoKnabben <91637195+BrunoKnabben@users.noreply.github.com>
2021-10-28 11:27:24 -0300

    Add files via upload

---
+++ Lista_de_tarefas.py
@@ -0,0 +1,51 @@
+def printa_titulo(texto='Insira o título', num_lin=7):
+    print('=' * num_lin, texto, '=' * num_lin)
+
+
+contador = 1
+tarefas = {}
+opcao = 0
+temp = {}
+
+while opcao != 5:
+    print('Selecione uma opção:\n')
+    print('[1] Adicionar tarefa')
+    print('[2] Listar tarefas')
+    print('[3] Undo')
+    print('[4] Redo')
+    print('[5] Encerrar')
+    try:
+        opcao = int(input())
+    except ValueError:
+        print('Opção inválida')
+    print()
+
+    if opcao == 1:
+        print(f'Tarefa {contador}: ', end='')
+        nova_tarefa = input()
+        tarefas[f'Tarefa {contador}'] = nova_tarefa
+        contador += 1
+
+    if opcao == 2:
+        if contador != 1:
+            printa_titulo('Lista de Tarefas')
+            for num_tarefa, descricao in tarefas.items():
+                print(f'{num_tarefa}: {descricao}')
+        else:
+            print('Ainda não há nenhuma tarefa.')
+        print()
+
+    if opcao == 3:
+        if contador == 1:
+            print('Não há nada para desfazer.\n')
+        else:
+            temp[f'Tarefa {contador - 1}'] = tarefas[f'Tarefa {contador - 1}']
+            del tarefas[f'Tarefa {contador - 1}']
+            contador -= 1
+
+    if opcao == 4:
+        if len(tarefas) != contador:
+            print('Não há nada para refazer\n')
+        else:
+            tarefas[f'Tarefa {contador}'] = temp[f'Tarefa {contador}']
+            contador += 1
